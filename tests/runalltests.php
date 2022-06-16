e54436661d1062256f42db2abacee62f2df49f9e
Michael Kliewe <info@phpgangsta.de>
2022-06-16 17:44:42 +0200

    Added GitHub Action for running PHPUnit tests Removed TRAVIS-checks from runalltests.php, and always use PHPUnit from composer

--- tests/runalltests.php
+++ tests/runalltests.php
@@ -20,34 +20,11 @@
  * @version    $Id$
  */
 
-$PHPUNIT = null;
-if (!$PHPUNIT) {
-    if (!$PHPUNIT && strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-        $PHPUNIT = `for %i in (phpunit.bat) do @echo.   %~\$PATH:i)`;
-    } else {
-        $PHPUNIT = trim(`echo \$PHPUNIT`);
-        if ( empty($PHPUNIT) ) {
-            $PHPUNIT = `which phpunit`;
-            $PHPUNIT = trim($PHPUNIT);
-        }
-    }
 
-    $PHPUNIT = trim($PHPUNIT);
-    if (!$PHPUNIT) {
-        echo "PHPUnit was not found on your OS!" . PHP_EOL;
-        exit(1);
-    }
-}
-
+$PHPUNIT = __DIR__ . '/../bin/phpunit';
 if (!is_executable($PHPUNIT)) {
     echo "PHPUnit is not executable ($PHPUNIT)";
-}
-
-if ($_SERVER['TRAVIS_PHP_VERSION'] == '5.2') {
-    //PHPUnit from git clone
-    $PHPUNIT = 'php -d include_path=\'.:./phpunit/phpunit/:./phpunit/dbunit/:./phpunit/php-code-coverage/:./phpunit/php-file-iterator/:./phpunit/php-invoker/:./phpunit/php-text-template/:./phpunit/php-timer:./phpunit/php-token-stream:./phpunit/phpunit-mock-objects/:./phpunit/phpunit-selenium/:./phpunit/phpunit-story/:/usr/local/lib/php\' ./phpunit/phpunit/phpunit.php';
-} else {
-    $PHPUNIT = '../bin/phpunit'; //PHPUnit from composer
+    exit;
 }
 
 // locate all tests
@@ -56,14 +33,10 @@ sort($files);
 
 // we'll capture the result of each phpunit execution in this value, so we'll know if something broke
 $result = 0;
+$failedFiles = [];
 
 // run through phpunit
 foreach ($files as $file) {
-    if ($_SERVER['TRAVIS_PHP_VERSION'] == 'hhvm' && $file == 'Zend/CodeGenerator/AllTests.php') {
-        echo "Skipping $file on HHVM" . PHP_EOL; //gets stuck on the HHVM
-        continue;
-    }
-
     echo "Executing {$file}" . PHP_EOL;
     system($PHPUNIT . ' --stderr -d memory_limit=-1 -d error_reporting=E_ALL\&E_STRICT -d display_errors=1 ' . escapeshellarg($file), $c_result);
     echo PHP_EOL;
@@ -72,9 +45,17 @@ foreach ($files as $file) {
     if ($c_result) {
         echo PHP_EOL . "Result of $file is $c_result" . PHP_EOL . PHP_EOL;
         $result = $c_result;
+        $failedFiles[] = $file;
     }
 }
 
 
 echo PHP_EOL . "All done. Result: $result" . PHP_EOL;
+
+if ($result) {
+    sort($failedFiles);
+    echo PHP_EOL . count($failedFiles) . ' test files failed: ' . PHP_EOL . PHP_EOL;
+    echo implode(PHP_EOL, $failedFiles) . PHP_EOL . PHP_EOL;
+}
+
 exit($result);
